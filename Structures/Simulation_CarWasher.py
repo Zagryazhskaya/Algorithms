1b5bab6801eda9f9899f8b57d7eba65ac2a35d45
Nataliia Zahriazhskaya <nzagryajskaya@gmail.com>
2018-02-25 12:02:07 +0300

    simulation updates

--- Structures/Simulation_CarWasher.py
+++ Structures/Simulation_CarWasher.py
@@ -1,51 +1,72 @@
 from pythonds.basic.queue import Queue
+
 import random
 
 class Washer:
-    def __init__(self,ppm):
+    def __init__(self, ppm):
         self.carerateerate = ppm
         self.currentTask = None
         self.timeRemaining = 0
-    def tisk(self):
+
+    def tick(self):
         if self.currentTask != None:
             self.timeRemaining = self.timeRemaining - 1
-    def buzy(self):
+            if self.timeRemaining <= 0:
+                self.currentTask = None
+
+    def busy(self):
         if self.currentTask != None:
             return True
         else:
             return False
+
     def startNext(self,newtask):
         self.currentTask = newtask
-        self.timeRemaining = newtask.getCars() * 60 / self.carerateerate
-
-
-
+        self.timeRemaining = newtask.getCars() * 60/self.carerateerate
 
 class Task:
-    def __init__(self,task):
-        ...
+    def __init__(self,time):
+        self.timestamp = time
+        self.cars = random.randrange(1,21)
+
     def getStamp(self):
-        ...
+        return self.timestamp
+
     def getCars(self):
-        ...
+        return self.cars
 
-    def simulation(numSeconds, carsPerMinute):
-        w = Washer(carsPerMinute)
-        washQueue = Queue()
-        waitingtimes = []
+    def waitTime(self, currenttime):
+        return currenttime - self.timestamp
 
-        for currentSecond in range(numSeconds):
 
-            if newCar():
-                task = Task(currentSecond)
-                washQueue.enqueue(task)
+def simulation(numSeconds, carsPerMinute):
 
-            if (not w.busy()) and (not washQueue.isEmpty()):
-                nexttask = washQueue.dequeue()
-                waitingtimes.append(nexttask.waitTime(currentSecond))
-                w.startNext(nexttask)
+    w = Washer(carsPerMinute)
+    washQueue = Queue()
+    waitingtimes = []
 
-            w.tick()
+    for currentSecond in range(numSeconds):
 
-        averageWait = sum(waitingtimes) / len(waitingtimes)
-        print("Average Wait %6.2f secs %3d tasks remaining." % (averageWait, washQueue.size()))
+      if newCar():
+         task = Task(currentSecond)
+         washQueue.enqueue(task)
+
+      if (not w.busy()) and (not washQueue.isEmpty()):
+        nexttask = washQueue.dequeue()
+        waitingtimes.append( nexttask.waitTime(currentSecond))
+        w.startNext(nexttask)
+
+      w.tick()
+
+    averageWait=sum(waitingtimes)/len(waitingtimes)
+    print("Average Wait %6.2f secs %3d tasks remaining."%(averageWait,washQueue.size()))
+
+def newCar():
+    num = random.randrange(1,181)
+    if num == 180:
+        return True
+    else:
+        return False
+
+for i in range(10):
+    simulation(3600,5)
