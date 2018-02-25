b9c6628ec4cd6950547a28e8e5af944e5cbd40b0
Nataliia Zahriazhskaya <nzagryajskaya@gmail.com>
2018-02-25 11:48:26 +0300

    add tisk and buzy methods, fixed problrm in function tisk

--- Structures/Simulation_CarWasher.py
+++ Structures/Simulation_CarWasher.py
@@ -7,12 +7,17 @@ class Washer:
         self.currentTask = None
         self.timeRemaining = 0
     def tisk(self):
-        if self.currentTask == None:
+        if self.currentTask != None:
             self.timeRemaining = self.timeRemaining - 1
     def buzy(self):
-        ...
-    def startNext(self):
-        ...
+        if self.currentTask != None:
+            return True
+        else:
+            return False
+    def startNext(self,newtask):
+        self.currentTask = newtask
+        self.timeRemaining = newtask.getCars() * 60 / self.carerateerate
+
 
 class Task:
     def __init__(self,task):
